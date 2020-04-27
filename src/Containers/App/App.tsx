8e7dce17c8758641fd4dc98f094ef572c7c4cc21
Raisa Primerova <riyuhide@gmail.com>
2020-04-27 11:33:53 -0600

    Add fetch restaurants functionality

--- src/Containers/App/App.tsx
+++ src/Containers/App/App.tsx
@@ -1,9 +1,35 @@
 import './App.scss';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { getRestaurants } from '_apiCalls/apiCalls';
+import { addRestaurants } from 'redux_utils/actions';
 
 const App: React.FC = () => {
+  const dispatch = useDispatch();
+  const [ error, setError ] = useState<string>('');
+  const [ loaded, setLoaded ] = useState<boolean>(false);
+  const fetchRestaurants = async (): Promise<void> => {
+    try {
+      const restaurants = await getRestaurants();
+      dispatch(addRestaurants(restaurants));
+      setLoaded(true);
+    }
+    catch (error) {
+      setLoaded(false);
+      setError(error);
+    }
+  };
+
+  const addRestaurantsToStore = (): void => { fetchRestaurants() };
+
+  useEffect(addRestaurantsToStore, []);
+
   return (
-    <div className="app"></div>
+    <div className="app">
+      { !loaded && <p>Loading ...</p> }
+      { loaded && <p>Loading is ended!</p>}
+      { error !== '' && <p>{ error }</p> }
+    </div>
   );
 }
 
