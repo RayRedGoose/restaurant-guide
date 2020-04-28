d713de31ceebb20effdc7104d9f696040708f4f8
Raisa Primerova <riyuhide@gmail.com>
2020-04-28 11:39:30 -0600

    Test action creators for page reducers

--- src/redux_utils/actions/actions.test.ts
+++ src/redux_utils/actions/actions.test.ts
@@ -16,3 +16,29 @@ describe("Add all restaurants function", () => {
     expect(result).toEqual(expected);
   });
 });
+
+describe("Change current page function", () => {
+  it("should return object with a type of CHANGE_CURRENT_PAGE when changeCurrentPage is called", () => {
+    const expected: I.IChangeCurrentPageAction = {
+      type: 'CHANGE_CURRENT_PAGE',
+      changer: 1
+    };
+
+    const result = actions.changeCurrentPage(1);
+
+    expect(result).toEqual(expected);
+  });
+});
+
+describe("Add max page function", () => {
+  it("should return object with a type of ADD_MAX_PAGES when addMaxPages is called", () => {
+    const expected: I.IAddMaxPages = {
+      type: 'ADD_MAX_PAGES',
+      total: 20
+    };
+
+    const result = actions.addMaxPages(20);
+
+    expect(result).toEqual(expected);
+  });
+});
