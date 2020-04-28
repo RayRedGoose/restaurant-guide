5cb94f87a7c6ca9eb1926e61af4d313872036540
Raisa Primerova <riyuhide@gmail.com>
2020-04-28 11:39:03 -0600

    Add action creators for tracking pages

--- src/redux_utils/actions/index.ts
+++ src/redux_utils/actions/index.ts
@@ -1,9 +1,21 @@
 import {
   IRestaurantObject,
-  IAddRestaurantAction
+  IAddRestaurantAction,
+  IChangeCurrentPageAction,
+  IAddMaxPages
 } from 'assets/ts/interfaces';
 
 export const addRestaurants = (restaurants: IRestaurantObject[]): IAddRestaurantAction => ({
   type: 'ADD_RESTAURANTS',
   restaurants: restaurants
+});
+
+export const changeCurrentPage = (changer: number): IChangeCurrentPageAction => ({
+  type: 'CHANGE_CURRENT_PAGE',
+  changer: changer
+});
+
+export const addMaxPages = (total: number): IAddMaxPages => ({
+  type: 'ADD_MAX_PAGES',
+  total: total
 });
