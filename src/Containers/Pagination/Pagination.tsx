0c246d8104fc765fa6b8d0ab6e7e20c65c49b461
Raisa Primerova <riyuhide@gmail.com>
2020-04-28 11:36:08 -0600

    Add Pagination component

---
+++ src/Containers/Pagination/Pagination.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { IAppStore } from 'assets/ts/interfaces';
+import { changeCurrentPage } from 'redux_utils/actions'
+
+const Pagination: React.FC = () => {
+  const dispatch = useDispatch();
+  const { displayPage, maxPages } = useSelector((store: IAppStore) => ({
+    displayPage: store.currentPage + 1,
+    maxPages: store.maxPages
+  }));
+
+  const decreaseCounter = (): void => {
+    if (displayPage > 1) dispatch(changeCurrentPage(-1));
+  };
+
+  const increaseCounter = () : void => {
+    if (displayPage < maxPages) dispatch(changeCurrentPage(1));
+  }
+
+  return (
+    <footer>
+      { displayPage > 1 &&
+        <button
+          id="prev-page-btn"
+          onClick={() => decreaseCounter()}> - </button>
+      }
+      <p className="pages">{`${displayPage} / ${maxPages}`}</p>
+      { displayPage < maxPages &&
+        <button
+          id="next-page-btn"
+          onClick={() => increaseCounter()}> + </button>}
+    </footer>
+  );
+};
+
+export default Pagination;
