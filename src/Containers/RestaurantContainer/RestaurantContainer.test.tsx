cba382d8ab53b8f1c3dc098c1dc41836da8253e6
Raisa Primerova <riyuhide@gmail.com>
2020-04-28 11:44:10 -0600

    Test pagination functionality in RestaurantContainer component

--- src/Containers/RestaurantContainer/RestaurantContainer.test.tsx
+++ src/Containers/RestaurantContainer/RestaurantContainer.test.tsx
@@ -1,14 +1,41 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import mockRestaurant from 'assets/ts/test/mockRestaurant';
-import RestaurantContainer from './RestaurantContainer';
+/**
+* @jest-environment jsdom
+*/
 
+import React from 'react';
+import mockStore from 'assets/ts/test/mockStore';
+import RestaurantContainer from './RestaurantContainer';
+import { render, act, RenderResult } from '@testing-library/react';
+
+const mockDispatch = jest.fn();
+let mockCurrentPage: number = 0;
+let mockDisplayPage: number = mockCurrentPage + 1;
 
 jest.mock('react-redux', () => ({
-  useSelector: () => [mockRestaurant]
+  useSelector: () => ({
+    ...mockStore,
+    currentPage: mockCurrentPage,
+    displayPage: mockDisplayPage
+  }),
+  useDispatch: () => mockDispatch
 }));
 
-it('should match the snapshot', () => {
-  const wrapper = shallow(<RestaurantContainer />);
-  expect(wrapper).toMatchSnapshot();
+describe("RestaurantContainer", () => {
+  let wrapper: RenderResult;
+
+  it('should match the snapshot', async () => {
+    await act(async () => {
+      wrapper = render(<RestaurantContainer />);
+    });
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should match the snapshot with the next pack of restaurants', async () => {
+    mockCurrentPage = 1;
+    mockDisplayPage = 2;
+    await act(async () => {
+      wrapper = render(<RestaurantContainer />);
+    });
+    expect(wrapper).toMatchSnapshot();
+  });
 });
