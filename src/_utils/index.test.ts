15588f0e69474a40cf79c1ae0d9b1a0350d0384f
Raisa Primerova <riyuhide@gmail.com>
2020-04-27 18:35:38 -0600

    Add alphabetically sorting util function

--- src/_utils/index.test.ts
+++ src/_utils/index.test.ts
@@ -1,3 +1,11 @@
-it('should match true', () => {
-  expect(true).toEqual(true);
+import * as utils from '../_utils';
+import { IRestaurantObject } from 'assets/ts/interfaces';
+import mockRestaurant, { mockRestaurantAdditional } from 'assets/ts/test/mockRestaurant';
+
+it('should return the array of restaurants sorted alphabetically by name', () => {
+  const restaurants: IRestaurantObject[] = [ mockRestaurant, mockRestaurantAdditional ];
+  const expected: IRestaurantObject[] = [ mockRestaurantAdditional, mockRestaurant ];
+  const result: IRestaurantObject[] = utils.sortByAlphabet(restaurants);
+
+  expect(result).toEqual(expected);
 });
--- src/assets/ts/test/mockRestaurant.ts
+++ src/assets/ts/test/mockRestaurant.ts
@@ -17,4 +17,21 @@ const mockRestaurant: IRestaurantObject = {
   attire: "business casual"
 };
 
+export const mockRestaurantAdditional: IRestaurantObject = {
+  "id": "00b35e1a-82b1-4988-b8b9-6df826db2818",
+  "name": "Matsuhisa",
+  "address1": "303 E Main St",
+  "city": "Aspen",
+  "state": "CO",
+  "zip": "81611",
+  "lat": "39.190723",
+  "long": "-106.82031",
+  "telephone": "(970) 544-6628",
+  "tags": "Social,Food and Dining,Restaurants,Japanese,Social,Food and Dining,Restaurants,Sushi",
+  "website": "http://www.matsuhisaaspen.com",
+  "genre": "Japanese,Sushi,Asian,Contemporary,Seafood",
+  "hours": "Open Daily 5:30 PM-9:00 PM",
+  "attire": "business casual"
+};
+
 export default mockRestaurant;
