4060020d437b27b148ea9a4e84472291c867c587
Raisa Primerova <riyuhide@gmail.com>
2020-04-30 12:25:08 -0600

    Add filtering utils functions

--- src/_utils/index.ts
+++ src/_utils/index.ts
@@ -1,31 +1,61 @@
-import { IRestaurantObject } from 'assets/ts/interfaces';
+import { IRestaurantObject, IFilters } from 'assets/ts/interfaces';
 
-export const sortByAlphabet = (restaurants: IRestaurantObject[]) => {
-  return restaurants.sort((a: IRestaurantObject, b: IRestaurantObject) => (
-    a.name > b.name ? 1 : -1
+export const sortByAlphabet = (restaurants: IRestaurantObject[]) => restaurants.sort((a: IRestaurantObject, b: IRestaurantObject) => (
+  a.name > b.name ? 1 : -1
+));
+
+export const getGenres = (restaurants: IRestaurantObject[]): string[] => restaurants
+  .map((rest: IRestaurantObject) => rest.genre)
+  .reduce((acc: string[], genres: string) => {
+    const separated = genres.split(',');
+    separated.forEach((genre: string) => {
+      if (!acc.includes(genre.toLowerCase())) { acc = [...acc, genre.toLowerCase()]; }
+    });
+    return acc;
+  }, []);
+
+export const getAttire = (restaurants: IRestaurantObject[]): string[] => restaurants
+  .map((rest: IRestaurantObject) => rest.attire)
+  .reduce((acc: string[], genres: string) => {
+    const separated = genres.split(' ');
+    separated.forEach((genre: string) => {
+      if (!acc.includes(genre.toLowerCase())) { acc = [...acc, genre.toLowerCase()]; }
+    });
+    return acc;
+  }, []);
+
+export const checkEmptyFilters = (filters: IFilters): boolean => {
+  const {
+    searchFilter, stateFilter, genreFilter, attireFilter,
+  } = filters;
+  return (
+    stateFilter !== ''
+    || genreFilter !== ''
+    || attireFilter !== ''
+    || searchFilter !== ''
+  );
+};
+
+export const checkMatches = (place: string, filter: string): boolean => place.toLowerCase().includes(filter.toLowerCase());
+
+export const checkState = (place: string, filter: string): boolean => (
+  (filter !== '') ? place === filter : true
+);
+
+export const applySearch = (rest: IRestaurantObject, query: string): boolean => (
+  checkMatches(rest.name, query)
+    || checkMatches(rest.city, query)
+    || checkMatches(rest.genre, query)
+);
+
+export const applyFilters = (rests: IRestaurantObject[], filters: IFilters): IRestaurantObject[] => {
+  const {
+    searchFilter, stateFilter, genreFilter, attireFilter,
+  } = filters;
+  return rests.filter((rest: IRestaurantObject) => (
+    applySearch(rest, searchFilter)
+    && checkState(rest.state, stateFilter)
+    && checkMatches(rest.genre, genreFilter)
+    && checkMatches(rest.attire, attireFilter)
   ));
-};
-
-export const getGenres = (restaurants: IRestaurantObject[]): string[] => {
-  return restaurants
-    .map((rest: IRestaurantObject) => rest.genre)
-    .reduce((acc: string[], genres: string) => {
-      const separated = genres.split(',');
-      separated.forEach((genre: string) => {
-        if(!acc.includes(genre.toLowerCase())) { acc = [...acc, genre.toLowerCase()] }
-      });
-      return acc;
-    }, []);
-};
-
-export const getAttire = (restaurants: IRestaurantObject[]): string[] => {
-  return restaurants
-    .map((rest: IRestaurantObject) => rest.attire)
-    .reduce((acc: string[], genres: string) => {
-      const separated = genres.split(' ');
-      separated.forEach((genre: string) => {
-        if(!acc.includes(genre.toLowerCase())) { acc = [...acc, genre.toLowerCase()] }
-      });
-      return acc;
-    }, []);
 };
