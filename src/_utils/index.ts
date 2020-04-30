f2841843384e42a0b3f2c00419565aca0a834df2
Raisa Primerova <riyuhide@gmail.com>
2020-04-29 18:58:02 -0600

    Add and test util function to get genres and attires as arrays

--- src/_utils/index.ts
+++ src/_utils/index.ts
@@ -5,3 +5,27 @@ export const sortByAlphabet = (restaurants: IRestaurantObject[]) => {
     a.name > b.name ? 1 : -1
   ));
 };
+
+export const getGenres = (restaurants: IRestaurantObject[]): string[] => {
+  return restaurants
+    .map((rest: IRestaurantObject) => rest.genre)
+    .reduce((acc: string[], genres: string) => {
+      const separated = genres.split(',');
+      separated.forEach((genre: string) => {
+        if(!acc.includes(genre.toLowerCase())) { acc = [...acc, genre.toLowerCase()] }
+      });
+      return acc;
+    }, []);
+};
+
+export const getAttire = (restaurants: IRestaurantObject[]): string[] => {
+  return restaurants
+    .map((rest: IRestaurantObject) => rest.attire)
+    .reduce((acc: string[], genres: string) => {
+      const separated = genres.split(' ');
+      separated.forEach((genre: string) => {
+        if(!acc.includes(genre.toLowerCase())) { acc = [...acc, genre.toLowerCase()] }
+      });
+      return acc;
+    }, []);
+};
