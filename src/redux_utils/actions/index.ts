203cf8496ab750c41136ef217fc476c921653556
Raisa Primerova <riyuhide@gmail.com>
2020-04-29 18:50:20 -0600

    Add action creators for filters

--- src/redux_utils/actions/index.ts
+++ src/redux_utils/actions/index.ts
@@ -1,21 +1,62 @@
-import {
-  IRestaurantObject,
-  IAddRestaurantAction,
-  IChangeCurrentPageAction,
-  IAddMaxPages
-} from 'assets/ts/interfaces';
+import * as I from 'assets/ts/interfaces';
 
-export const addRestaurants = (restaurants: IRestaurantObject[]): IAddRestaurantAction => ({
+export const addRestaurants = (restaurants: I.IRestaurantObject[]): I.IAddRestaurantAction => ({
   type: 'ADD_RESTAURANTS',
   restaurants: restaurants
 });
 
-export const changeCurrentPage = (changer: number): IChangeCurrentPageAction => ({
+export const addGenres = (genres: string[]): I.IAddGenresAction => ({
+  type: 'ADD_GENRES',
+  genres: genres
+});
+
+export const addAttires = (attires: string[]): I.IAddAttiresAction => ({
+  type: 'ADD_ATTIRES',
+  attires: attires
+});
+
+export const changeCurrentPage = (changer: number): I.IChangeCurrentPageAction => ({
   type: 'CHANGE_CURRENT_PAGE',
   changer: changer
 });
 
-export const addMaxPages = (total: number): IAddMaxPages => ({
+export const addMaxPages = (total: number): I.IAddMaxPagesAction => ({
   type: 'ADD_MAX_PAGES',
   total: total
+});
+
+export const addStateFilter = (state: string): I.IAddStateFilterAction => ({
+  type: 'ADD_STATE_FILTER',
+  state: state
+});
+
+export const removeStateFilter = (): I.IRemoveStateFilterAction => ({
+  type: 'REMOVE_STATE_FILTER'
+});
+
+export const addGenreFilter = (genre: string): I.IAddGenreFilterAction => ({
+  type: 'ADD_GENRE_FILTER',
+  genre: genre
+});
+
+export const removeGenreFilter = (): I.IRemoveGenreFilterAction => ({
+  type: 'REMOVE_GENRE_FILTER'
+});
+
+export const addAttireFilter = (attire: string): I.IAddAttireFilterAction => ({
+  type: 'ADD_ATTIRE_FILTER',
+  attire: attire
+});
+
+export const removeAttireFilter = (): I.IRemoveAttireFilterAction => ({
+  type: 'REMOVE_ATTIRE_FILTER'
+});
+
+export const addSearchFilter = (search: string): I.IAddSearchFilterAction => ({
+  type: 'ADD_SEARCH_FILTER',
+  search: search
+});
+
+export const removeSearchFilter = (): I.IRemoveSearchFilterAction => ({
+  type: 'REMOVE_SEARCH_FILTER'
 });
