cf0f6b5f21ce0aa0b04375e0749e4a6756245a7e
Raisa Primerova <riyuhide@gmail.com>
2020-04-29 18:53:38 -0600

    Add and test stateFilter reducer

---
+++ src/redux_utils/reducers/stateFilter/stateFilter.test.ts
@@ -0,0 +1,44 @@
+import stateFilterReducer from './stateFilter';
+import {
+  IActionObject,
+  IAddStateFilterAction,
+  IRemoveStateFilterAction
+} from 'assets/ts/interfaces';
+
+describe('stateFilterReducer', () => {
+  it("should return initial value", () => {
+    const mockAction: IActionObject = {
+      type: '',
+      state: undefined
+    };
+    const expected: string = '';
+    const result = stateFilterReducer(undefined, mockAction);
+
+    expect(result).toEqual(expected);
+  });
+
+  it("should return new state if type of action is ADD_STATE_FILTER", () => {
+    const mockAction: IAddStateFilterAction = {
+      type: 'ADD_STATE_FILTER',
+      state: 'CO'
+    };
+
+    const expected: string = 'CO';
+
+    const result = stateFilterReducer('', mockAction);
+
+    expect(result).toEqual(expected);
+  });
+
+  it("should return empty string if type of action is REMOVE_STATE_FILTER", () => {
+    const mockAction: IRemoveStateFilterAction = {
+      type: 'REMOVE_STATE_FILTER'
+    };
+
+    const expected: string = '';
+
+    const result = stateFilterReducer('CO', mockAction);
+
+    expect(result).toEqual(expected);
+  });
+});
