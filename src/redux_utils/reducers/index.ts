522c976f45455799b40c4ea41f6ca907c1e06697
Raisa Primerova <riyuhide@gmail.com>
2020-04-29 18:57:07 -0600

    Add filter reducers to store

--- src/redux_utils/reducers/index.ts
+++ src/redux_utils/reducers/index.ts
@@ -2,11 +2,23 @@ import { combineReducers } from 'redux';
 import restaurants from './restaurants/restaurants';
 import currentPage from './currentPage/currentPage';
 import maxPages from './maxPages/maxPages';
+import genres from './genres/genres';
+import attires from './attires/attires';
+import stateFilter from './stateFilter/stateFilter';
+import genreFilter from './genreFilter/genreFilter';
+import attireFilter from './attireFilter/attireFilter';
+import searchFilter from './searchFilter/searchFilter';
 
 const rootReducer = combineReducers({
   restaurants,
   currentPage,
-  maxPages
+  maxPages,
+  genres,
+  attires,
+  stateFilter,
+  genreFilter,
+  attireFilter,
+  searchFilter
 });
 
 export default rootReducer;
