9ab0dcacdcff855405bc65c19f94dd42723d70b3
Raisa Primerova <riyuhide@gmail.com>
2020-04-29 18:58:02 -0600

    Add and test util function to get genres and attires as arrays

--- src/_utils/index.test.ts
+++ src/_utils/index.test.ts
@@ -9,3 +9,28 @@ it('should return the array of restaurants sorted alphabetically by name', () =>
 
   expect(result).toEqual(expected);
 });
+
+it('should return the array of all genres', () => {
+  const restaurants: IRestaurantObject[] = [ mockRestaurant, mockRestaurantAdditional ];
+  const expected: string[] = [
+    'steak',
+    'american',
+    'contemporary',
+    'seafood',
+    'cafe',
+    'japanese',
+    'sushi',
+    'asian'
+  ];
+  const result: string[] = utils.getGenres(restaurants);
+
+  expect(result).toEqual(expected);
+});
+
+it('should return the array of all attires', () => {
+  const restaurants: IRestaurantObject[] = [ mockRestaurant, mockRestaurantAdditional ];
+  const expected: string[] = [ 'business', 'casual' ];
+  const result: string[] = utils.getAttire(restaurants);
+
+  expect(result).toEqual(expected);
+});
