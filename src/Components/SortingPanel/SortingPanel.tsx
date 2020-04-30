3ab5b309ccf9ab5ad4759b50221ad660df9e1ac5
Raisa Primerova <riyuhide@gmail.com>
2020-04-30 12:26:06 -0600

    Add SearchForm component to SortingPanel component

--- src/Components/SortingPanel/SortingPanel.tsx
+++ src/Components/SortingPanel/SortingPanel.tsx
@@ -1,15 +1,15 @@
 import './SortingPanel.scss';
 import React from 'react';
 import Filter from 'Containers/Filter/Filter';
+import SearchForm from 'Containers/SearchForm/SearchForm';
 
-const SortingPanel: React.FC = () => {
-  return (
-    <header className="sorting-panel">
-      <Filter type='states' />
-      <Filter type='genres' />
-      <Filter type='attires' />
-    </header>
-  );
-}
+const SortingPanel: React.FC = () => (
+  <header className="sorting-panel">
+    <SearchForm />
+    <Filter type="states" />
+    <Filter type="genres" />
+    <Filter type="attires" />
+  </header>
+);
 
 export default SortingPanel;
