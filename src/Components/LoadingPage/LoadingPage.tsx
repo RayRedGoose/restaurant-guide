eb30ab37bae72cd96f4db722ff2df9266d02ef3e
Raisa Primerova <riyuhide@gmail.com>
2020-04-30 18:34:47 -0600

    Create LoadingPage component

---
+++ src/Components/LoadingPage/LoadingPage.tsx
@@ -0,0 +1,14 @@
+import './LoadingPage.scss';
+import React from 'react';
+import loading from 'assets/images/loading.svg';
+
+const LoadingPage: React.FC = () => {
+  return (
+    <section className="loading">
+      <img src={loading} alt="loading"/>
+      <h2>Loading...</h2>
+    </section>
+  );
+};
+
+export default LoadingPage;
