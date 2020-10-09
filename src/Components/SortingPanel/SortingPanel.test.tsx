089b92ae7858eacf0bf4470598b51d2a3a0a5ff4
Raisa Primerova <riyuhide@gmail.com>
2020-10-08 21:31:51 -0600

    Update SortingPanel tests

--- src/Components/SortingPanel/SortingPanel.test.tsx
+++ src/Components/SortingPanel/SortingPanel.test.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import SortingPanel from './SortingPanel';
+/**
+ * @jest-environment jsdom
+ */
 
-it('should match the snapshot', () => {
+import React from "react";
+import { shallow } from "enzyme";
+import SortingPanel from "./SortingPanel";
+
+const mockDispatch = jest.fn();
+jest.mock("react-redux", () => ({
+  useDispatch: () => mockDispatch,
+  useSelector: () => "",
+}));
+
+it("should match the snapshot", () => {
   const wrapper = shallow(<SortingPanel />);
+
   expect(wrapper).toMatchSnapshot();
 });
