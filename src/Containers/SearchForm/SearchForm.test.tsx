04412f2b17e0287a936098e0e55256447475d293
Raisa Primerova <riyuhide@gmail.com>
2020-10-08 21:29:03 -0600

    Update SearchForm tests

--- src/Containers/SearchForm/SearchForm.test.tsx
+++ src/Containers/SearchForm/SearchForm.test.tsx
@@ -2,17 +2,18 @@
  * @jest-environment jsdom
  */
 
-import React from 'react';
-import { mount } from 'enzyme';
-import SearchForm from './SearchForm';
-import { addSearchFilter, removeSearchFilter, removeAllFilters } from 'redux_utils/actions';
+import React from "react";
+import { mount } from "enzyme";
+import SearchForm from "./SearchForm";
+import { addSearchFilter, removeSearchFilter } from "redux_utils/actions";
 
 const mockDispatch = jest.fn();
-jest.mock('react-redux', () => ({
-  useDispatch: () => mockDispatch
+jest.mock("react-redux", () => ({
+  useDispatch: () => mockDispatch,
+  useSelector: () => "",
 }));
 
-jest.mock('redux_utils/actions');
+jest.mock("redux_utils/actions");
 
 describe("SearchForm", () => {
   let wrapper: any;
@@ -21,56 +22,104 @@ describe("SearchForm", () => {
     wrapper = mount(<SearchForm />);
   });
 
-  it('should match the snapshot', () => {
+  it("should match the snapshot", () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should update input state when change event occur', () => {
-    const input = wrapper.find('input[type="text"]');
-    expect(input.getDOMNode().value).toEqual('');
-
-    input.simulate('change', {
-      target: { value: 'hi!' }
+  it("should match the snapshot if input is not empty", () => {
+    const input = wrapper.find("input");
+    input.simulate("change", {
+      target: { value: "query" },
     });
-    expect(input.getDOMNode().value).toEqual('hi!');
+
+    expect(wrapper).toMatchSnapshot();
   });
 
-  it('should call removeSearchFilter when change event fired with empty value', () => {
-    const input = wrapper.find('input[type="text"]');
-    input.simulate('change', {
-      target: { value: '' }
+  it("should change class of input if it's on focus", () => {
+    const input = wrapper.find("input");
+    expect(input.getDOMNode()).toHaveClass("closed");
+
+    input.simulate("focus");
+
+    expect(input.getDOMNode()).toHaveClass("opened");
+  });
+
+  it("should change class of input if it's not empty", () => {
+    const input = wrapper.find("input");
+    expect(input.getDOMNode()).toHaveClass("closed");
+
+    input.simulate("change", {
+      target: { value: "query" },
     });
+
+    expect(input.getDOMNode()).toHaveClass("opened");
+  });
+
+  it("should update input state when change event occur", () => {
+    const input = wrapper.find("input");
+    expect(input.getDOMNode().value).toEqual("");
+
+    input.simulate("change", {
+      target: { value: "hi!" },
+    });
+
+    expect(input.getDOMNode().value).toEqual("hi!");
+  });
+
+  it("should call removeSearchFilter when change event fired with empty value", () => {
+    const input = wrapper.find("input");
+    input.simulate("change", {
+      target: { value: "" },
+    });
+
     expect(removeSearchFilter).toHaveBeenCalled();
   });
 
-  it('should call addSearchFilter when form is submitted', () => {
-    wrapper.find('input[type="text"]').simulate('change', {
-      target: { value: 'query' }
+  it("should call addSearchFilter when form is submitted", () => {
+    const input = wrapper.find("input");
+    input.simulate("change", {
+      target: { value: "query" },
     });
-    wrapper.find('form').simulate('submit');
-    expect(addSearchFilter).toHaveBeenCalledWith('query');
+
+    wrapper.find("form").simulate("submit");
+
+    expect(addSearchFilter).toHaveBeenCalledWith("query");
   });
 
-  it('should call addSearchFilter when submit button is clicked', () => {
-    wrapper.find('input[type="text"]').simulate('change', {
-      target: { value: 'query' }
+  it("should call addSearchFilter when submit button is clicked", () => {
+    const input = wrapper.find("input");
+    input.simulate("change", {
+      target: { value: "query" },
     });
-    wrapper.find('input[type="submit"]').simulate('click');
-    expect(addSearchFilter).toHaveBeenCalledWith('query');
+
+    wrapper.find(".search-btn").simulate("click");
+
+    expect(addSearchFilter).toHaveBeenCalledWith("query");
   });
 
-  it('should call removeAllFilters when click button is clicked', () => {
-    wrapper.find('button').simulate('click');
-    expect(removeAllFilters).toHaveBeenCalled();
+  it("should call removeSearchFilter when clean button is clicked", () => {
+    const input = wrapper.find("input");
+    input.simulate("change", {
+      target: { value: "hi!" },
+    });
+
+    const icon = wrapper.find(".remove-btn").at(0);
+    icon.simulate("click");
+
+    expect(removeSearchFilter).toHaveBeenCalled();
   });
 
-  it('should clean input value after clean button is clicked', () => {
+  it("should clean input value after clean button is clicked", () => {
     const input = wrapper.find('input[type="text"]');
-    input.simulate('change', {
-      target: { value: 'hi!' }
+    input.simulate("change", {
+      target: { value: "hi!" },
     });
-    expect(input.getDOMNode().value).toEqual('hi!');
-    wrapper.find('button').simulate('click');
-    expect(input.getDOMNode().value).toEqual('');
+
+    expect(input.getDOMNode().value).toEqual("hi!");
+
+    const icon = wrapper.find(".remove-btn").at(0);
+    icon.simulate("click");
+
+    expect(input.getDOMNode().value).toEqual("");
   });
 });
