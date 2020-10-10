5140508be49cd9738bfde56d20ca172075ad4d62
Raisa Primerova <riyuhide@gmail.com>
2020-10-09 21:35:18 -0600

    Set rerendering of ClearFiltersButton after filter is change

--- src/Containers/CleanFIltersButton/CleanFIltersButton.tsx
+++ src/Containers/CleanFIltersButton/CleanFIltersButton.tsx
@@ -1,17 +1,48 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { removeAllFilters } from "redux_utils/actions";
+import { IAppStore } from "assets/ts/interfaces";
 
 const CleanFIltersButton: React.FC = () => {
+  const [isDisplayed, setIsDisplayed] = useState<boolean>(false);
   const dispatch = useDispatch();
+  const { attireFilter, genreFilter, stateFilter, searchFilter } = useSelector(
+    (store: IAppStore) => ({
+      attireFilter: store.attireFilter,
+      genreFilter: store.genreFilter,
+      searchFilter: store.searchFilter,
+      stateFilter: store.stateFilter,
+    })
+  );
 
   const cleanFilters = (): void => {
     dispatch(removeAllFilters());
   };
 
-  return (
+  const checkEmptyFilters = (): boolean => {
+    const checkAttire: boolean = attireFilter === "";
+    const checkGenre: boolean = genreFilter === "";
+    const checkSearch: boolean = searchFilter === "";
+    const checkState: boolean = stateFilter === "";
+
+    return checkAttire && checkGenre && checkSearch && checkState;
+  };
+
+  const rerendering = () => {
+    const isEmpty: boolean = checkEmptyFilters();
+    return !isEmpty ? setIsDisplayed(true) : setIsDisplayed(false);
+  };
+
+  useEffect(rerendering, [
+    attireFilter,
+    genreFilter,
+    stateFilter,
+    searchFilter,
+  ]);
+
+  return isDisplayed ? (
     <button
       className="clean-btn"
       onClick={cleanFilters}
@@ -19,6 +50,8 @@ const CleanFIltersButton: React.FC = () => {
     >
       <FontAwesomeIcon icon={faTimes} />
     </button>
+  ) : (
+    <div />
   );
 };
 
