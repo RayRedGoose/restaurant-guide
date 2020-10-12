1d8288fbe05ccdf11b5276f282396acf6878c26e
Raisa Primerova <riyuhide@gmail.com>
2020-10-12 17:44:15 -0600

    Update snapshots because of changing RestaurantCard structure

--- src/Containers/RestaurantCard/RestaurantCard.test.tsx
+++ src/Containers/RestaurantCard/RestaurantCard.test.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import mockRestaurant from 'assets/ts/test/mockRestaurant';
-import RestaurantCard from './RestaurantCard';
-import mockStore from 'assets/ts/test/mockStore';
+import React from "react";
+import { shallow } from "enzyme";
+import mockRestaurant from "assets/ts/test/mockRestaurant";
+import RestaurantCard from "./RestaurantCard";
+import mockStore from "assets/ts/test/mockStore";
 
-jest.mock('react-redux', () => ({
-  useSelector: () => mockStore
+jest.mock("react-redux", () => ({
+  useSelector: () => mockStore,
 }));
 
-it('should match the snapshot if it\'s not clicked', () => {
-  const wrapper = shallow(
-    <RestaurantCard restaurant={mockRestaurant} />
-  );
+it("should match the snapshot if it's not clicked", () => {
+  const wrapper = shallow(<RestaurantCard restaurant={mockRestaurant} />);
   expect(wrapper).toMatchSnapshot();
 });
 
-it('should match the snapshot if it\'s clicked', () => {
-  const wrapper = shallow(
-    <RestaurantCard restaurant={mockRestaurant} />
-  );
-  wrapper.find('.restaurant-card').simulate('click');
+it("should match the snapshot if it's clicked", () => {
+  const wrapper = shallow(<RestaurantCard restaurant={mockRestaurant} />);
+  wrapper.find("footer").simulate("click");
   expect(wrapper).toMatchSnapshot();
 });
